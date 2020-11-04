b852ddf524a14c03214a2311d794752867c9d3e3
IlhamLamp <ilhamutomo24@gmail.com>
2020-11-04 10:43:50 +0700

    upload ulang gambar 16.17.18

--- Lab-2/lab2errorfix.py
+++ Lab-2/lab2errorfix.py
@@ -2,10 +2,28 @@ a = input("Masukkan nilai a: ")
 b = input("Masukkan nilai b: ")
 print("Variabel a=",a)
 print("Variabel b=",b)
-print("Hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+print("Hasil penggabungan {1}&{0}=%s".format(a,b) %(b+a))
 
 #konversi nilai variabel
 a = int(a)
 b = int(b)
 print("Hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
-print("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+print("Hasil pembagian {1}/{0}=%d".format(a,b) %(b/a))
+
+"""
+Karena jika saya menjalankan program pada baris ke-11
+
+    print("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+
+Jika :
+a adalah 10
+b adalah 15
+
+maka b/a=%d
+    15/10=0.666 >> ini bukan hasilnya, karena 15/10 adalah 1.5
+
+sedangkan bila a/b=%d
+    10/15=0.666
+
+Jadi saya menukar %(a/b) menjadi %(b/a)
+"""
